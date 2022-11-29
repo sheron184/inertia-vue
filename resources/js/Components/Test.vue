f542de54873770a02a56ee06e2ad285ce579d025
sheron184 <sheronjude4@gmail.com>
2022-11-29 11:24:34 +0530

    INITIAL SETUP OF LARAVEL AND INERTIA WITH VUE3

---
+++ resources/js/Components/Test.vue
@@ -0,0 +1,16 @@
+<script>
+import { Link } from '@inertiajs/inertia-vue3';
+export default{
+    components:{
+        Link
+    }
+}
+</script>
+<template lang="">
+    <div>
+        <Link href="/profile">Profile</Link>
+    </div>
+</template>
+<style lang="">
+    
+</style>
